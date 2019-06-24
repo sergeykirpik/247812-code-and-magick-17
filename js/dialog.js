523d85b0833bd7826e39f484801ccd94b89c20e0
Sergey Kirpik <sergeykirpik@gmail.com>
2019-06-24 03:18:04 +0300

    Перетаскивание артефактов: начало

--- js/dialog.js
+++ js/dialog.js
@@ -2,47 +2,13 @@
 
 (function () {
 
-  function makeDraggable(el, dragHandle) {
-    var startX;
-    var startY;
-    var dragging = false;
-    function dragHandlePreventDefault(evt) {
-      evt.preventDefault();
-    }
-    function mouseDownHandler(evt) {
-      evt.preventDefault();
-      startX = evt.clientX;
-      startY = evt.clientY;
-      document.addEventListener('mousemove', mouseMoveHandler);
-      document.addEventListener('mouseup', mouseUpHandler);
-      dragHandle.addEventListener('click', dragHandlePreventDefault);
-    }
-    function mouseMoveHandler(evt) {
-      evt.preventDefault();
-      el.style.left = el.offsetLeft + (evt.clientX - startX) + 'px';
-      el.style.top = el.offsetTop + (evt.clientY - startY) + 'px';
-      startX = evt.clientX;
-      startY = evt.clientY;
-      dragging = true;
-    }
-    function mouseUpHandler(evt) {
-      evt.preventDefault();
-      document.removeEventListener('mousemove', mouseMoveHandler);
-      document.removeEventListener('mouseup', mouseUpHandler);
-      if (!dragging) {
-        dragHandle.removeEventListener('click', dragHandlePreventDefault);
-      }
-      dragging = false;
-    }
-
-    dragHandle.addEventListener('mousedown', mouseDownHandler);
-  }
+  var util = window.util;
 
   var dialog = document.querySelector('.setup');
-  dialog.classList.remove('hidden');
-
   var dragHandle = dialog.querySelector('.upload');
+  util.makeDraggable(dialog, dragHandle);
 
-  makeDraggable(dialog, dragHandle);
+  var shopItem = document.querySelector('.setup-artifacts-cell img');
+  util.makeDraggable(shopItem, shopItem);
 
 })();
