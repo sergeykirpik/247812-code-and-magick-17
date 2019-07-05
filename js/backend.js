e2b15044656f729dfdf967d8ee8e209909422529
Sergey Kirpik <sergeykirpik@gmail.com>
2019-07-05 13:00:26 +0200

    Класс для окна с ошибками. Рефакторинг

--- js/backend.js
+++ js/backend.js
@@ -5,13 +5,15 @@
   var SAVE_FORM_DATA_URL = 'https://js.dump.academy/code-and-magick';
   var DEFAULT_XHR_TIMEOUT = 5000;
 
+  function noop() {}
+
   function Loader(method, url) {
     var loader = this;
     var xhr = new XMLHttpRequest();
     xhr.timeout = DEFAULT_XHR_TIMEOUT;
     xhr.open(method, url);
-    this.onSuccess = null;
-    this.onError = null;
+    this.onSuccess = noop;
+    this.onError = noop;
 
     xhr.addEventListener('load', function () {
       if (xhr.status === 200) {
@@ -19,28 +21,20 @@
         try {
           data = JSON.parse(xhr.responseText);
         } catch (err) {
-          if (loader.onError) {
-            loader.onError('Плохой формат ответа сервера: ' + err);
-          }
+          loader.onError('Плохой формат ответа сервера: ' + err);
         }
-        if (loader.onSuccess) {
-          loader.onSuccess(data);
-        }
+        loader.onSuccess(data);
       } else {
         loader.onError('Ошибка с кодом ' + xhr.status);
       }
     });
 
     xhr.addEventListener('error', function () {
-      if (loader.onError) {
-        loader.onError('Ошибка соединения');
-      }
+      loader.onError('Ошибка соединения');
     });
 
     xhr.addEventListener('timeout', function () {
-      if (loader.onError) {
-        loader.onError('Таймаут соединения');
-      }
+      loader.onError('Таймаут соединения');
     });
     this.xhr = xhr;
   }
@@ -51,20 +45,14 @@
   window.backend = {
     load: function (onLoad, onError) {
       var loader = new Loader('GET', GET_SIMILAR_WIZARDS_URL);
-      loader.onSuccess = onLoad;
-      loader.onError = onError;
+      loader.onSuccess = onLoad || noop;
+      loader.onError = onError || noop;
       loader.send();
     },
     save: function (formData, onLoad, onError) {
       var loader = new Loader('POST', SAVE_FORM_DATA_URL);
-      loader.onSuccess = function (data) {
-        onLoad(data);
-      };
-      loader.onError = function (err) {
-        if (onError) {
-          onError(err);
-        }
-      };
+      loader.onSuccess = onLoad || noop;
+      loader.onError = onError || noop;
       loader.send(formData);
     }
   };
--- js/msg.js
+++ js/msg.js
@@ -3,14 +3,12 @@
 (function () {
   var AnimationController = window.animation.AnimationController;
 
-  var MSG_STYLE = 'text-align: center; position: absolute; color: white; font-size: 30px; left: 50%; top: 30%; transform: translate(-50%, 0%); z-index: 100; padding: 10px 20px; border: 3px solid white; border-radius: 10px;';
   var MSG_TIMEOUT = 4000;
   var FADE_DURATION = 800;
 
-  var showMessage = function (text, color) {
+  var showMessage = function (text, alertClass) {
     var el = document.createElement('div');
-    el.style = MSG_STYLE;
-    el.style.background = color;
+    el.className = 'alert ' + alertClass;
     el.textContent = text;
     el.style.opacity = 0.0;
 
@@ -38,10 +36,10 @@
 
   window.msg = {
     success: function (text) {
-      showMessage(text, 'green');
+      showMessage(text, 'alert-success');
     },
     error: function (text) {
-      showMessage(text, 'red');
+      showMessage(text, 'alert-error');
     }
   };
 })();
